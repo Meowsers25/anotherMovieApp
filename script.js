143406842fa5c353b41482930acd4b9589167ab4
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-07-18 00:15:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+// npm install --save express ejs request
+
+
+// var express = require("express");
+// var app = require("express")();//
+// var request = require("request");
+// app.set("view engine", "ejs");
+
+// app.get("/", function(req, res) {
+//   res.render("search");
+// });
+
+// app.get("/results", function(req, res) {
+//     var query = req.query.search;
+//     var url = "http://omdbapi.com/?s" + query + "&apikey=thewdb";
+    
+//     request(url, function(error, response, body) {
+//         if(!error && response.statusCode == 200) {
+//             var data = JSON.parse(body);
+//             res.render("results", {data: data});
+//         }
+//     });
+// });
+
+
+// app.listen(process.env.PORT, process.env.IP, function() {
+//   console.log("Movie app has started!"); 
+// });
+
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+   res.render("search"); 
+});
+
+app.get("/results", function(req, res){
+   request("http://www.omdbapi.com/?s=star%20wars&apikey=thewdb", function(error, response, body) {
+       if(!error && response.statusCode == 200) {
+           var data = JSON.parse(body);
+           res.render("results", {data: data});
+       }
+   }); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Movie App has started"); 
+});
