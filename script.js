1479710feb09ec18ced89acf759ea6e16503d58e
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-07-18 00:25:41 +0000

    working on files

--- script.js
+++ script.js
@@ -39,7 +39,9 @@ app.get("/", function(req, res) {
 });
 
 app.get("/results", function(req, res){
-   request("http://www.omdbapi.com/?s=star%20wars&apikey=thewdb", function(error, response, body) {
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb";
+   request(url, function(error, response, body) {
        if(!error && response.statusCode == 200) {
            var data = JSON.parse(body);
            res.render("results", {data: data});
